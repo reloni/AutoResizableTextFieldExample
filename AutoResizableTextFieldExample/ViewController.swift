48d752c15416191c991c9a89680a6d6e78c7df4b
Anton Efimenko <reloni@ya.ru>
2018-01-20 18:56:25 +0300

    Working example

---
+++ AutoResizableTextFieldExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoResizableTextFieldExample
+//
+//  Created by Anton Efimenko on 20.01.2018.
+//  Copyright © 2018 Anton Efimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
